2d9ee4c5cbf3ec50a8f709404389a335cff6323c
ccazuc <clementorus@hotmail.fr>
2018-01-01 13:35:19 +0100

    Specular update

--- src/light_specular.c
+++ src/light_specular.c
@@ -4,7 +4,7 @@ void	add_specular(t_env *env, t_collision *collision, t_vector *normal, t_vector
 {
 	int		value;
 
-	value = cos(dabs(vector_angle(&collision->ray.dir, normal) - vector_angle(light_dir, normal)))
+	value = pow(cos(dabs(vector_angle(&collision->ray.dir, normal) - vector_angle(light_dir, normal))), 200)
 	collision->color.r = dmin(255, collision->color.r + value);
 	collision->color.g = dmin(255, collision->color.g + value);
 	collision->color.b = dmin(255, collision->color.b + value);
