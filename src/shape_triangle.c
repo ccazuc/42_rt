194cd129ea146c459b2d32b0140104b2a4ecce96
Ctsu27 <kehuang42student@gmail.com>
2019-01-14 15:16:04 +0100

    refrac: norme on shape_triangle.c

--- src/shape_triangle.c
+++ src/shape_triangle.c
@@ -6,35 +6,45 @@
 /*   By: ccazuc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/29 12:19:15 by ccazuc            #+#    #+#             */
-/*   Updated: 2018/11/29 17:47:14 by ccazuc           ###   ########.fr       */
+/*   Updated: 2019/01/14 15:15:09 by kehuang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
 
+static t_vector	mul_vec_tri(t_vector const a, t_vector const b)
+{
+	t_vector	n;
+
+	n.x = a.y * b.z;
+	n.y = a.z * b.x;
+	n.z = a.x * b.y;
+	return (n);
+}
+
+static void	get_determinant(t_ray const *ray, t_object const *object,
+		t_triangle_collision *tri_coll)
+{
+	tri_coll->e1 = sub_vector(object->rot, object->pos);
+	tri_coll->e2 = sub_vector(object->p3, object->pos);
+	tri_coll->p = sub_vector(mul_vec_tri(ray->dir, tri_coll->e2),
+			mul_vec_tri(tri_coll->e2, ray->dir));
+	tri_coll->det = dot_product(&tri_coll->e1, &tri_coll->p);
+}
+
 void	get_triangle_normal(t_vector *vector, t_object *object,
-t_collision *collision)
+		t_collision *collision)
 {
 	t_vector	u;
 	t_vector	v;
 	double		angle;
 
-	u.x = object->rot.x - object->pos.x;
-	u.y = object->rot.y - object->pos.y;
-	u.z = object->rot.z - object->pos.z;
-	v.x = object->p3.x - object->pos.x;
-	v.y = object->p3.y - object->pos.y;
-	v.z = object->p3.z - object->pos.z;
-	vector->x = u.y * v.z - u.z * v.y;
-	vector->y = u.z * v.x - u.x * v.z;
-	vector->z = u.x * v.y - u.y * v.x;
+	u = sub_vector(object->rot, object->pos);
+	v = sub_vector(object->p3, object->pos);
+	*vector = sub_vector(mul_vec_tri(u, v), mul_vec_tri(v, u));
 	angle = vector_angle(vector, &collision->dir) * 180. / M_PI;
 	if (angle <= 90)
-	{
-		vector->x = -vector->x;
-		vector->y = -vector->y;
-		vector->z = -vector->z;
-	}
+		*vector = mul_vector(*vector, -1.0f);
 }
 
 void	collide_triangle(t_ray *ray, t_object *object,
@@ -43,39 +53,24 @@ t_collision *collision, t_object *previous_object)
 	t_triangle_collision	tri_coll;
 	double					factor;
 
-	tri_coll.e1.x = object->rot.x - object->pos.x;
-	tri_coll.e1.y = object->rot.y - object->pos.y;
-	tri_coll.e1.z = object->rot.z - object->pos.z;
-	tri_coll.e2.x = object->p3.x - object->pos.x;
-	tri_coll.e2.y = object->p3.y - object->pos.y;
-	tri_coll.e2.z = object->p3.z - object->pos.z;
-	tri_coll.p.x = ray->dir.y * tri_coll.e2.z - ray->dir.z * tri_coll.e2.y;
-	tri_coll.p.y = ray->dir.z * tri_coll.e2.x - ray->dir.x * tri_coll.e2.z;
-	tri_coll.p.z = ray->dir.x * tri_coll.e2.y - ray->dir.y * tri_coll.e2.x;
-	tri_coll.det = dot_product(&tri_coll.e1, &tri_coll.p);
+	get_determinant(ray, object, &tri_coll);
 	if (tri_coll.det == 0)
 		return ;
 	tri_coll.det = 1. / tri_coll.det;
-	tri_coll.t.x = ray->pos.x - object->pos.x;
-	tri_coll.t.y = ray->pos.y - object->pos.y;
-	tri_coll.t.z = ray->pos.z - object->pos.z;
+	tri_coll.t = sub_vector(ray->pos, object->pos);
 	tri_coll.u = dot_product(&tri_coll.t, &tri_coll.p) * tri_coll.det;
 	if (tri_coll.u < 0.00001 || tri_coll.u > 1.00001)
 		return ;
-	tri_coll.q.x = tri_coll.t.y * tri_coll.e1.z - tri_coll.t.z * tri_coll.e1.y;
-	tri_coll.q.y = tri_coll.t.z * tri_coll.e1.x - tri_coll.t.x * tri_coll.e1.z;
-	tri_coll.q.z = tri_coll.t.x * tri_coll.e1.y - tri_coll.t.y * tri_coll.e1.x;
+	tri_coll.q = sub_vector(mul_vec_tri(tri_coll.t, tri_coll.e1),
+			mul_vec_tri(tri_coll.e1, tri_coll.t));
 	tri_coll.v = dot_product(&ray->dir, &tri_coll.q) * tri_coll.det;
 	if (tri_coll.v < 0.00001 || tri_coll.u + tri_coll.v > 1.00001)
 		return ;
 	factor = dot_product(&tri_coll.e2, &tri_coll.q) * tri_coll.det;
-	if (factor >= collision->distance || factor <= 0)
+	if (factor >= collision->distance || factor <= 0
+			|| (object == previous_object && factor < 0.0001))
 		return ;
-	if (object == previous_object && factor < 0.0001)
-		return ;
-	collision->pos.x = ray->pos.x + ray->dir.x * factor;
-	collision->pos.y = ray->pos.y + ray->dir.y * factor;
-	collision->pos.z = ray->pos.z + ray->dir.z * factor;
+	collision->pos = add_vector(mul_vector(ray->dir, (float)factor), ray->pos);
 	collision->object = object;
 	collision->distance = factor;
 }
