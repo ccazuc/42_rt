ea32cefcf4ed91e2a7affa838956a9114bb9ca07
Ctsu27 <kehuang42student@gmail.com>
2019-01-17 19:54:39 +0100

    feat: texture check board ambient

--- src/get_pixel_color.c
+++ src/get_pixel_color.c
@@ -6,7 +6,7 @@
 /*   By: ccazuc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/17 15:48:47 by ccazuc            #+#    #+#             */
-/*   Updated: 2019/01/14 15:52:06 by kehuang          ###   ########.fr       */
+/*   Updated: 2019/01/17 19:53:19 by kehuang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,31 @@ static float		add_color(float color, double factor)
 	return (color * factor);
 }
 
-static unsigned int	check_ambient(t_env *env, t_object *object,
+static unsigned int	check_ambient(t_env *env, t_collision *hit,
 		unsigned int color)
 {
 	t_color_mask	mask;
+	t_color_mask	obj_clr;
 
+	if (hit->object->has_texture)
+		obj_clr = texture_checkboard(hit->pos, hit->object->rot,
+				hit->object->texu_offs, hit->object->texu_size);
+	else
+	{
+		obj_clr.r = hit->object->color_r;
+		obj_clr.g = hit->object->color_g;
+		obj_clr.b = hit->object->color_b;
+	}
 	if (get_color_r(color) < get_color_r(env->light_ambient))
-		mask.r = add_color(object->color_r, env->ambient_power);
+		mask.r = add_color(obj_clr.r, env->ambient_power);
 	else
 		mask.r = get_color_r(color);
 	if (get_color_g(color) < get_color_g(env->light_ambient))
-		mask.g = add_color(object->color_g, env->ambient_power);
+		mask.g = add_color(obj_clr.g, env->ambient_power);
 	else
 		mask.g = get_color_g(color);
 	if (get_color_b(color) < get_color_b(env->light_ambient))
-		mask.b = add_color(object->color_b, env->ambient_power);
+		mask.b = add_color(obj_clr.b, env->ambient_power);
 	else
 		mask.b = get_color_b(color);
 	return (conv_rgb_to_int(mask.r, mask.g, mask.b));
@@ -55,7 +65,7 @@ unsigned int		get_pixel_color(t_env *env, t_ray *ray,
 			&& collision.object)
 	{
 		color = get_light_color(env, &collision, recursion);
-		check_ambient(env, collision.object, color);
+		color = check_ambient(env, &collision, color);
 	}
 	return (color);
 }
