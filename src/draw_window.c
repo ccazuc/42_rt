53bf56f7830c0b6fc476c81fd39f1ef62a5e125e
Ctsu27 <kehuang42student@gmail.com>
2019-01-24 11:40:23 +0100

    feat: red cross

--- src/draw_window.c
+++ src/draw_window.c
@@ -6,13 +6,20 @@
 /*   By: ccazuc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/15 14:52:30 by ccazuc            #+#    #+#             */
-/*   Updated: 2019/01/14 14:21:26 by kehuang          ###   ########.fr       */
+/*   Updated: 2019/01/24 11:35:45 by kehuang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
 
-void	fill_ray(t_env *env, t_ray *ray, int x, int y)
+static int	suicide(void *p)
+{
+	(void)p;
+	exit(0);
+	return (0);
+}
+
+void		fill_ray(t_env *env, t_ray *ray, int x, int y)
 {
 	int	width;
 	int	height;
@@ -30,7 +37,7 @@ void	fill_ray(t_env *env, t_ray *ray, int x, int y)
 	vector_normalize(&ray->dir);
 }
 
-void	render(t_env **env, int i, char should_hook)
+void		render(t_env **env, int i, char should_hook)
 {
 	env[i]->render_start = epoch_millis();
 	create_thread(env[i]);
@@ -39,4 +46,5 @@ void	render(t_env **env, int i, char should_hook)
 	if (i == 0)
 		mlx_loop_hook(env[i]->mlx_ptr, loop_handler, env);
 	mlx_hook(env[i]->mlx_win, 3, 2, key_up_handler, env[i]);
+	mlx_hook(env[i]->mlx_win, 17, 1L << 17, suicide, env[i]);
 }
